12d27436f4bd4c7c509ed81c8563ff62f8e95324
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-05-04 10:20:23 +0500

    add fixed menu

---
+++ components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    <button @click="increment">increment</button>
+    {{ count }}
+    <button @click="decrement">decrement</button>
+  </div>
+</template>
+<script setup>
+const count = ref(0)
+function increment () {
+  count.value++
+}
+function decrement () {
+  count.value--
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
 })
